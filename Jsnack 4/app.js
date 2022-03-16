79de2ac09a4ccc2ef8acd16a70beb74285b73d85
NuvoloniM <nuvoloni.m@gmail.com>
2022-03-16 17:01:39 +0100

    JSnack 4

---
+++ Jsnack 4/app.js
@@ -0,0 +1,34 @@
+/*
+Snack4 (Bonus)
+Crea due array che hanno un numero di elementi diversi.  
+Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+*/
+
+//creo due array
+let listA = ["mele", "pere", "banane", "fragole", "more", "lamponi", "ananas"];
+let listB = ["rosso", "blu", "giallo", "grigio", "rosa"];
+
+
+// let x = parseInt(listA.length) - parseInt(listB.length);
+
+//mi chiedo se list a > b
+if (listA.length > listB.length) {
+    // imposto x come la differenza tra le due lunghezze => sarà per quante volte ripeto il ciclo
+    let x = listA.length - listB.length
+    for (let i = 0; i < x; i++) {
+        //chiedo cosa inserire nella lista più corta
+        element = prompt('Scrivi un elemento da inserire in una lista');
+        listB.push(element);
+    }
+    //ripeto nel caso opposto
+} else {
+    let x = listB.length - listA.length
+    for (let i = 0; i < x; i++) {
+        element = prompt('Scrivi un elemento da inserire in una lista');
+        listA.push(element);
+    }
+    
+}
+
+console.log(listA);
+console.log(listB);
